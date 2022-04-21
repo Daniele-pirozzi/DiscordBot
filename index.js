7f8d91d505b0464381b89f5c8bd7cca61b6a2418
Daniele Pirozzi <85101043+Daniele-pirozzi@users.noreply.github.com>
2022-04-21 17:34:54 +0000

    slowly adding features and fixing errors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 const Database = require("@replit/database")
 const db = new Database()
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 db.get("rolls").then(rolls => {
   if (!rolls || rolls.length < 1) {
@@ -15,27 +16,74 @@ db.get("responding").then(value => {
   }  
 })
 
-
-client.on("ready", () => {
+client.on("ready", msg => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", msg => {
-  if (msg.content.includes("ping")) {
-    msg.reply("pong");
-  }
+// client.on('interactionCreate', async interaction => {
+// 	if (!interaction.isCommand()) return;
 
+// 	if (interaction.commandName === 'ping') {
+// 		const row = new MessageActionRow()
+// 			.addComponents(
+
+        
+// 			);
+
+// 		const embed = new MessageEmbed()
+// 			.setColor('#0099ff')
+// 			.setTitle('Some title')
+// 			.setDescription();
+
+// 		await interaction.reply({ content: {} , ephemeral: true, embeds: [embed], components: [row] });
+// 	}
+// });
+
+client.on("message", msg => {
   if(msg.content.includes("cosa?") || msg.content.includes("che cosa?") || msg.content.includes("chi?")){
     msg.channel.send("STOCAZZO", {files: ["https://c.tenor.com/Ww4jX1oirBYAAAAM/marilyn-monroe-sto-cazzo.gif"] });
   }
-  
 
+  if(msg.content.includes("clara moment") || msg.content.includes("Clara moment")){
+    msg.channel.send("",{files: ["https://c.tenor.com/EaGblD_u2BAAAAAC/tefacciocorca-corcare.gif"] });
+  }
+
+  if(msg.content.includes("basito") || msg.content.includes("f4")){
+    msg.channel.send("",{files: ["https://c.tenor.com/eU8TztSCXysAAAAC/f4basito-boris.gif"] });    
+  }
+
+  if(msg.content.includes("kyne") ||
+      msg.content.includes("loric") || 
+      msg.content.includes("varination")||
+      msg.content.includes("nage") ||
+      msg.content.includes("dorcelnessness") ||
+      msg.content.includes("harfam"))
+  {
+    msg.channel.send("",{files: ["https://i.kym-cdn.com/photos/images/newsfeed/002/206/828/c67.jpg"] });   
+  }
+
+  if(msg.content.includes("pacchetto")){
+    msg.channel.send("è fisicamente un ...",{files: ["https://c.tenor.com/LMgcm-AwNKsAAAAS/frustrated-yelling.gif"] });  
+  }
+  
   if(msg.content.startsWith('bot')){
     var message = msg.content.split('bot ');
     var parsedMsg = parseIstruction(msg);
-    console.log("result ", JSON.stringify(parsedMsg.result), '\nSuccesses ', JSON.stringify(parsedMsg.successes), '\n');
-    msg.reply("result ", JSON.stringify(parsedMsg.result), '\n');
-    msg.reply('Successes ', JSON.stringify(parsedMsg.successes), '\n');
+    
+    var msg1 = JSON.stringify(parsedMsg.result);
+    console.log(msg1);
+    msg.reply("**result** " + msg1);
+
+    if(parsedMsg.successes > 0 ){
+      msg1 = JSON.stringify(parsedMsg.successes);
+      console.log(msg1);
+      msg.reply("**successes** " + msg1);
+      
+    }else if(msg.content.includes('chance')){
+      msg.reply("**Critical failure**");
+    }else if(!msg.content.includes('init')){
+      msg.reply("**Fail**");
+    }
   }
 })
 
@@ -70,10 +118,6 @@ function rollDice(successValue, diceNumber, explodesOn){
   }
 }
 
-// function chance(){
-  
-// }
-
 function rote(successValue, diceNumber, explodesOn){
   var i=0;
   var successes = 0;
@@ -81,7 +125,7 @@ function rote(successValue, diceNumber, explodesOn){
   var reroll = true;
   
   while(i<diceNumber){
-    const randomNumber = this.dice + this.dice + 2;
+    const randomNumber = Math.round((Math.random() * (3) + 1 + Math.random() * (3) +1)) + 2;
     results.push(randomNumber);
     
     if(randomNumber >= successValue){
@@ -91,6 +135,8 @@ function rote(successValue, diceNumber, explodesOn){
     }else if(reroll){
       reroll = false;
       diceNumber++;
+    }else{
+      reroll = true;
     }
     
     if(randomNumber === explodesOn) diceNumber++;
@@ -114,18 +160,9 @@ function rote(successValue, diceNumber, explodesOn){
   
 }
 
-// function Eagain(){
-  
-// }
-
-// function Nagain(){
-  
-// }
-
 function parseIstruction(msg){
   var message = msg.content.split(' ');
-  console.log(message);
-
+  
   switch (message[1]){
     case 'roll':
       var obj = rollDice(9, message[2], 10);
@@ -133,17 +170,29 @@ function parseIstruction(msg){
       return obj;
      
     case 'rote':
-      var obj = rote(9, message[2], 10);
+      var obj = rote(8, message[2], 10);
       return obj;
      
    case 'Nagain':
-      var obj = rollDice(9, message[2], 10);
+      var obj = rollDice(9, message[2], 9);
      return obj;
       
    case 'Eagain':
+      var obj = rollDice(8, message[2], 8);
+      return obj;
+      
    case 'init':
+      var obj = rollDice(11, 1, 11);
+      return obj;
+      
    case 'chance':
+      var obj = rollDice(11, 1, 11);
+      return obj;
   }
+}
+
+function printDbFile(){
+  
 }
 
 client.login(process.env.TOKEN)
